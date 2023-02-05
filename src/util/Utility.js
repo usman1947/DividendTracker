22555caa88687b3e9eb71b777cb5d0250ab5e876
Usman <usman22aslam@gmail.com>
2023-02-05 13:36:53 +0800

    adding functions to update objects in different data sets

--- src/util/Utility.js
+++ src/util/Utility.js
@@ -6,3 +6,16 @@ export function isNullOrEmpty(value){
     }
     return isNil(value) || isEmpty(value)
 }
+
+export function updateObjectInArrayById(array, predicate, updatedObj){
+    let tempArray = [...array]
+    let index = tempArray.findIndex(predicate);
+    tempArray[index] = updatedObj;
+    return tempArray;
+}
+
+export function updateObjectInMapByKey(map, key, updatedValue){
+    let newMap = new Map(map)
+    newMap.set(key, updatedValue)
+    return newMap
+}
