61fae0cf779229f04007181effc2d674672e0054
Usman <usman22aslam@gmail.com>
2023-02-05 19:14:59 +0800

    adding market data slice

--- src/store.js
+++ src/store.js
@@ -2,15 +2,17 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
 import testReducer from 'services/testSlice.js'
 import holdingSlice from 'services/holdingSlice.js'
-import { alphaAdvantageApi } from 'services/alphaAdvantage'
+import marketDataSlice from 'services/marketDataSlice.js'
+import { api } from 'services/api'
 
 export const store = configureStore({
   reducer: {
     test: testReducer,
     holdings: holdingSlice,
-    [alphaAdvantageApi.reducerPath]: alphaAdvantageApi.reducer
+    marketData: marketDataSlice,
+    [api.reducerPath]: api.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(alphaAdvantageApi.middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware)
 })
 
 setupListeners(store.dispatch)
