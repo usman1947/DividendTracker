71285b243fa2d0a239341e0354108509bb6059cb
Usman <usman22aslam@gmail.com>
2023-03-06 18:56:36 +0800

    adding packages

--- src/store.js
+++ src/store.js
@@ -1,16 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/dist/query'
 import testReducer from 'services/testSlice.js'
-import holdingSlice from 'services/holdingSlice.js'
-import marketDataSlice from 'services/marketDataSlice.js'
 import stocksDataSlice from 'services/stocksDataSlice.js'
 import { api } from 'services/api'
 
 export const store = configureStore({
   reducer: {
     test: testReducer,
-    holdings: holdingSlice,
-    marketData: marketDataSlice,
     stocksData: stocksDataSlice,
     [api.reducerPath]: api.reducer
   },
