65bd3acc4bf464502880205fb08c595ae73001f1
Usman <usman22aslam@gmail.com>
2023-03-11 14:24:03 +0800

    adding logo to header

--- src/common-components/Layout/Header.jsx
+++ src/common-components/Layout/Header.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Box } from "@mui/material";
+import { AppBar, Toolbar, Typography, Stack } from "@mui/material";
 import SearchStocks from 'stocks/components/SearchStocks.js'
 
 const Header = () => {
     return (
         <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
             <Toolbar variant="app-bar">
-                <Typography variant="h6">
-                    Dividend Tracker
-                </Typography>
+                <Stack direction='row' alignItems="center" spacing={2}>
+                    <img alt='logo' src={require('assets/logo.png')} style={{width: '50px'}}/>
+                    <Typography variant="h6">
+                        Dividend Tracker
+                    </Typography>
+                </Stack>
                 <SearchStocks
                 width='500px'
                 onChange={() => undefined}
                 />
-                <Box/>
+                <Stack/>
             </Toolbar>
         </AppBar>
     )
