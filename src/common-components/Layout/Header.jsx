ff96c963deac0279631a8ecfe4bf0ec4a713b2f5
Usman <usman22aslam@gmail.com>
2023-03-12 09:26:08 +0800

    Implementing swipeable drawer for mobile

--- src/common-components/Layout/Header.jsx
+++ src/common-components/Layout/Header.jsx
@@ -1,15 +1,42 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Stack, useMediaQuery, useTheme } from "@mui/material";
+import React, {useState} from "react";
+import { AppBar, Toolbar, Typography, Stack, useMediaQuery, useTheme, SwipeableDrawer, List, ListItemButton, ListItem, ListItemIcon, ListItemText, IconButton } from "@mui/material";
 import SearchStocks from 'stocks/components/SearchStocks.js'
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import { WebUrl } from 'util/Constants.js';
+import { useHistory, useLocation } from 'react-router-dom';
+import MenuIcon from '@mui/icons-material/Menu';
 
+const SideBarListItems = [
+    {
+        label: 'Dashboard',
+        url: WebUrl._DASHBOARD,
+        icon: DashboardIcon,
+    },
+    {
+        label: 'Portfolio',
+        url: WebUrl._PORTFOLIO,
+        icon: ShowChartIcon
+    },
+    {
+        label: 'WatchList',
+        url: 'watchList',
+        icon: WatchLaterIcon,
+        disabled: true
+    }
+]
 const Header = () => {
     const theme = useTheme()
     const isXsDevices = useMediaQuery(theme.breakpoints.down('sm'));
+    const _history = useHistory();
+    const location = useLocation();
+    const [open, setOpen] = useState(false);
     return (
         <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
             <Toolbar variant="app-bar">
                 <Stack direction='row' alignItems="center" spacing={2}>
-                    <img alt='logo' src={require('assets/logo.png')} style={{width: '50px'}}/>
+                    <img alt='logo' src={require('assets/logo.png')} style={{width: isXsDevices ? '40px' : '50px'}}/>
                     <Typography variant="h6">
                         Dividend Tracker
                     </Typography>
@@ -20,6 +47,41 @@ const Header = () => {
                 onChange={() => undefined}
                 />}
                 <Stack/>
+                {isXsDevices && 
+                <IconButton 
+                sx={{ mr: 3 }}
+                color="inherit" onClick={() => setOpen(true)}>
+                    <MenuIcon />
+                </IconButton>}
+                {isXsDevices &&
+                <SwipeableDrawer
+                open={open}
+                anchor='right'
+                onClose={()=> setOpen(false)}
+                >
+                <Toolbar/>
+                <List>
+                    {SideBarListItems.map((item) => {
+                        const isSelected = location.pathname?.substring(1) === item.url
+                        return(
+                            <ListItem key={item.label} disablePadding>
+                                <ListItemButton 
+                                onClick={() => {
+                                    setOpen(false);
+                                    _history.push(item.url)
+                                }} 
+                                selected={isSelected}
+                                disabled={item.disabled}
+                                >
+                                    <ListItemIcon>
+                                        {item.icon && <item.icon />}
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.label} />
+                                </ListItemButton>
+                            </ListItem>
+                    )})}
+                </List>
+                </SwipeableDrawer>}
             </Toolbar>
         </AppBar>
     )
