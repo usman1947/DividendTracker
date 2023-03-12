d374a273dbdb858012209dbbc25d1aa0190b9a3b
Usman <usman22aslam@gmail.com>
2023-03-12 10:34:48 +0800

    refactoring side menu

--- src/common-components/Layout/Header.jsx
+++ src/common-components/Layout/Header.jsx
@@ -1,36 +1,12 @@
 import React, {useState} from "react";
-import { AppBar, Toolbar, Typography, Stack, useMediaQuery, useTheme, SwipeableDrawer, List, ListItemButton, ListItem, ListItemIcon, ListItemText, IconButton } from "@mui/material";
+import { AppBar, Toolbar, Typography, Stack, useMediaQuery, useTheme, SwipeableDrawer, IconButton } from "@mui/material";
 import SearchStocks from 'stocks/components/SearchStocks.js'
-import ShowChartIcon from '@mui/icons-material/ShowChart';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import WatchLaterIcon from '@mui/icons-material/WatchLater';
-import { WebUrl } from 'util/Constants.js';
-import { useHistory, useLocation } from 'react-router-dom';
 import MenuIcon from '@mui/icons-material/Menu';
+import SideMenuListItems from "common-components/Layout/SideMenuListItems.jsx";
 
-const SideBarListItems = [
-    {
-        label: 'Dashboard',
-        url: WebUrl._DASHBOARD,
-        icon: DashboardIcon,
-    },
-    {
-        label: 'Portfolio',
-        url: WebUrl._PORTFOLIO,
-        icon: ShowChartIcon
-    },
-    {
-        label: 'WatchList',
-        url: 'watchList',
-        icon: WatchLaterIcon,
-        disabled: true
-    }
-]
 const Header = () => {
     const theme = useTheme()
     const isXsDevices = useMediaQuery(theme.breakpoints.down('sm'));
-    const _history = useHistory();
-    const location = useLocation();
     const [open, setOpen] = useState(false);
     return (
         <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
@@ -59,28 +35,7 @@ const Header = () => {
                 anchor='right'
                 onClose={()=> setOpen(false)}
                 >
-                <Toolbar/>
-                <List>
-                    {SideBarListItems.map((item) => {
-                        const isSelected = location.pathname?.substring(1) === item.url
-                        return(
-                            <ListItem key={item.label} disablePadding>
-                                <ListItemButton 
-                                onClick={() => {
-                                    setOpen(false);
-                                    _history.push(item.url)
-                                }} 
-                                selected={isSelected}
-                                disabled={item.disabled}
-                                >
-                                    <ListItemIcon>
-                                        {item.icon && <item.icon />}
-                                    </ListItemIcon>
-                                    <ListItemText primary={item.label} />
-                                </ListItemButton>
-                            </ListItem>
-                    )})}
-                </List>
+                <SideMenuListItems setOpen={setOpen}/>
                 </SwipeableDrawer>}
             </Toolbar>
         </AppBar>
