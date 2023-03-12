11007e00ba3a36d8bc863eec5b36bc67c4350f75
Usman <usman22aslam@gmail.com>
2023-03-12 09:13:43 +0800

    fixing portfolio's mobile UI

--- src/stocks/components/OutdatedDataDisclaimer.js
+++ src/stocks/components/OutdatedDataDisclaimer.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { IconButton, Stack, Typography } from '@mui/material';
+import { IconButton, Stack, Typography, useTheme, useMediaQuery } from '@mui/material';
 import { useLazyGetStocksDataQuery, useGetAllHoldingsQuery } from 'services/api'
 import AutoRenewIcon from '@mui/icons-material/Autorenew';
 
 const OutdatedDataDisclaimer = ({sx}) => {
-
+  const theme = useTheme()
+  const isXsDevices = useMediaQuery(theme.breakpoints.down('sm'));
   const holdingApi = useGetAllHoldingsQuery()
 	const [triggerGetStocksData, {isFetching: isFetchingStocksData}] = useLazyGetStocksDataQuery()
 
@@ -16,7 +17,8 @@ const OutdatedDataDisclaimer = ({sx}) => {
   return (
       <Stack direction='row' width='100%' spacing={1} alignItems='center' justifyContent='flex-end' sx={sx}>
         <Typography variant='subtitle2' color='secondary.main'>
-          *Disclaimer* Market data is not updated periodically, To get latest market data please click refresh
+          {isXsDevices ? 'Press here to refresh market data' : 
+          '*Disclaimer* Market data is not updated periodically, To get latest market data please click refresh'}
         </Typography>
         <IconButton disableRipple size='small' onClick={updateMarketData}
         disabled={isFetchingStocksData}>
