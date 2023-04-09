d12dc235ce265f34286c5331b8c0e9a113316602
Usman <usman22aslam@gmail.com>
2023-04-09 15:26:32 +0800

    adding styling for ant components

---
+++ src/styling/ant-design/theme.js
@@ -0,0 +1,10 @@
+import { theme as AntTheme } from 'antd'
+
+export const theme = {
+    algorithm: AntTheme.darkAlgorithm,
+    token: {
+        colorPrimary: '#646cff',
+        borderRadius: 16,
+        fontFamily: "'Quicksand', sans-serif",
+    },
+}
